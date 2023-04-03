1d52376e425b169b8cfc6c46ff650a494e0ffc1d
gokul355 <rgokul2962@gmail.com>
2023-04-03 12:13:44 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,19 @@ const { log } = require('console');
 const app = express()
 app.use(bodyParser.urlencoded({external:false}))
 app.use(express.json())
-app.get('/getData')
+app.get('/getData', (req,res,next) => {
+    const data = []
+    db.query( `select * from student `,(err,result) => {
+        data = result
+    } 
+    )
+    res.send(data)
+})
 
 app.post('/addNew',(req,res,next) => {
     const data = req.body.data
-    db.query(`insert into student(firstname.lastname,mailId,phoneno.gender,addressline1.addressline2,city,state,postalcode.country) values ()`)
+    db.query(`insert into student(firstname.lastname,mailId,phoneno.gender,addressline1.addressline2,city,state,postalcode.country) values (data.fname,data.lname,data.md,data.pno,data.dob,data,gender,data.adl1,data.adl2,data.cty,data.state,data.pc,data.cntry)`, (ree,result) => {
+        console.log(result);
+    })
 })
 app.listen(3000)
